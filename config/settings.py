b4c2e1b9071090f1f019bf9f99246f0bcdf72f7f
rodrigoamaro22004525 <a22004525@alunos.ulht.pt>
2022-05-26 14:47:15 +0000

    vdsv

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic', # novo
+    'whitenoise.runserver_nostatic',  # novo
     'django.contrib.staticfiles',
     'portfolio',
 ]
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # novo
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # novo
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_URL = '/portfolio/static/'    # substitua nome_aplicacao pelo nome da sua aplicação
+STATIC_URL = '/static/'
+# STATIC_URL = '/portfolio/static/'    # substitua nome_aplicacao pelo nome da sua aplicação
 # Default primary key field type
 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -131,5 +131,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))   # novo
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
