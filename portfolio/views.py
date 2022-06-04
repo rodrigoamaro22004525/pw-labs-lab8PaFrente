547ddedb4f8e2abfd810b305d130808c79f6fd8b
rodrigoamaro22004525 <a22004525@alunos.ulht.pt>
2022-06-04 11:06:36 +0000

    <YOUR COMMIT MESSAGE>

--- portfolio/views.py
+++ portfolio/views.py
@@ -132,7 +132,6 @@ def quizz_page_view(request):
         if request.method == 'POST':
             if form.is_valid():
                 form = form.save(commit=False)
-                form.name = user.username
                 form.save()
                 desenha_grafico_resultados(pontuacaoquizz.objects.all())
                 return HttpResponseRedirect(request.path_info)
