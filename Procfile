d35bd40596a15d64b6accb24eb82d1497eb7c082
rodrigoamaro22004525 <a22004525@alunos.ulht.pt>
2022-06-04 17:44:33 +0000

    bv

---
+++ Procfile
@@ -0,0 +1,4 @@
+web gunicorn config.wsgi --log-file -
+heroku login
+heroku keys:add #get SSL key
+heroku config:set DISABLE_COLLECTSTATIC=1
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ DEBUG = env.bool("DEBUG", default=False)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','portfolio-buildblock.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
