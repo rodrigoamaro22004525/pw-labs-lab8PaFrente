f5c23fa1c3276b060114e859f3376a09e47c7b46
rodrigoamaro22004525 <a22004525@alunos.ulht.pt>
2022-06-15 12:00:54 +0000

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -87,8 +87,6 @@ class noticias(models.Model):
         return self.titulo
 
 
-
-
 class tecnologias(models.Model):
     nome = models.CharField(max_length=50)
     acronimo = models.CharField(max_length=5)
@@ -97,7 +95,6 @@ class tecnologias(models.Model):
     logo = models.ImageField(upload_to='media/', null=True)
     linkYo = models.CharField(max_length=200, default="https://github.com/rodrigoamaro22004525?tab=repositories")
     descricao = models.CharField(max_length=400)
-    tipo = models.CharField(max_length=10)
 
     def __str__(self):
         return self.nome
